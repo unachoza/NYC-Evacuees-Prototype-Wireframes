d8b4d14629d7542758d4efcc6ab0b345dec6b511
Arianna Choza <unachoza@gmail.com>
2019-02-24 06:44:49 -0500

    starter code

---
+++ root/script.js
@@ -0,0 +1,13 @@
+const pageToInjury = () => {
+    window.location.href='./identifyInjury.html'
+    console.log('I was clicked')
+}
+
+const clickThis = () => {
+    console.log('I was clicked icon')
+}
+
+const nextCard = () => {
+    event.target.style.display = 'none'
+    console.log('did you disappear')
+}
